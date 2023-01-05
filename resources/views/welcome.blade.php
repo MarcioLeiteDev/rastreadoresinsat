59f62e8573512019bfa27141e7841a1cebb2f10d
MarcioLeiteDev <contato@marcioleitedev.com.br>
2023-01-05 17:18:11 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    
+    <script src="https://kit.fontawesome.com/204aa4dead.js" crossorigin="anonymous"></script>
+    <title>Insat Rastreadores Via Satélite</title>
+  </head>
+  <body>
+
+  <header class="header">
+    <img src="/image/logo.png" alt="" width="200">
+    <h3>Rastreamento Via Satélite <i class="fa-solid fa-satellite"></i></h3>
+  </header>
+   
+  <section class="layerOne">
+    <div class="box">
+    <h2>Insat Rastreadores</h2>
+        <h3>Alta Tecnologia Rastreamento Via Satélite <i class="fa-solid fa-satellite"></i> </h3>
+    </div>
+
+  </section>
+  <section class="layerTwo">
+    <div class="titulo">
+       
+        <ul>
+            <li> Ideal Para </li>
+            <li>Carros</li>
+            <li>Motos</li>
+            <li>Utilitários</li>
+            <li>Caminhão</li>
+            <li>Frotas</li>
+            <li>Pessoas</li>
+        </ul>
+        
+      </div>
+
+      <div class="titulo2">
+
+       
+     </div>
+     
+  </section>
+  <section class="layerThree">
+    <div class="blocoOne">
+    ...
+    </div>
+    <div class="blocoTwo">
+    <h3>A cada <strong>3 minutos</strong> um Veículo é <span>Roubado no Brasil<span></h3>
+        <h3>Aumente até 95% as chances de <span>Recuperação do seu veículo</span> em Caso de Roubo ou Furto</h3>
+    </div>        
+  </section>
+  <section class="layerfour">
+    <div class="blocoUm"> 
+        <h3>Quais Vantagens do Rastreador Insat</h3>
+    </div>
+    <div class="blocoDois">
+        <ul>
+            <li> <i class="fa-solid fa-check"></i> Alarme local anti-furto , Seja informado se seu veículo for ligado sem sua permissão, via SMS em seu celular .</li>
+            <li> <i class="fa-solid fa-check"></i> Localização veículo em tempo real via APP</li>
+            <li> <i class="fa-solid fa-check"></i> Histórico de Translado e Velocidade</li>
+            <li> <i class="fa-solid fa-check"></i> Localização em tempo real via mapa Google(StreetView) veja a fachada da rua, local exato do seu veículo :)</li>
+            <li> <i class="fa-solid fa-check"></i> Central de monitoramento e suporte para recuperação de veículo 24Hs</li>
+        </ul>
+    </div>
+
+  </section>
+
+  <section class="layerFive">
+<div class="blocoimg"> 
+
+</div>
+
+<div class="bloco">
+<h3>Assistência 24 Hs</h3>
+    <h5>Opcional</h5>
+    <ul>
+        <li><i class="fa-solid fa-check"></i> Socorro Guincho</li>
+        <li><i class="fa-solid fa-check"></i> Soccoro Mecânico</li>
+        <li><i class="fa-solid fa-check"></i> Socorro Borracheiro</li>
+        <li><i class="fa-solid fa-check"></i> Pane Seca</li>
+    </ul>
+</div>
+  </section>
+
+
+  <section class="layerSix">
+    <div class="bloco">
+        <h2>Veja Como Funciona o sistema</h2>
+    </div>
+    <div class="video">
+    <div class="embed-responsive embed-responsive-16by9">
+    <iframe  src="https://www.youtube.com/embed/Pk0kVff_cjk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+    </div>
+
+  </section>
+
+  <section class="layerSeven">
+    <h3>Veja O que dizem sobre o Rastreador Insat</h3>
+    <div class="sec">
+        <div class="bloco">
+            <div class="imgMarcio"></div>
+            <h3>Marcio Leite - SP</h3>
+            <p>Equipamento funciona perfeito, sempre tenho a localização em tempo real do meu veículo.</p>
+        </div>
+        <div class="bloco">
+            <div class="imgPedro"></div>
+            <h3>Pedro Braz - SP</h3>
+            <p>Meus funcionários sai com carro da empresa para serviços externos, economizei quase 40% em combustível depois que comecei monitorar eles, por onde eles estão passando.</p>
+        </div>
+        <div class="bloco">
+            <div class="imgWaldir"></div>
+            <h3>Waldir Ratinni - SP</h3>
+            <p>Trabalho com carro na rua com equipamentos caros no carro, deixei carro frente um cliente, quando meu celular tocou informando que meu carro tinha sido violado, fui ver cadê meu carro !!!, liguei imediatamente para policia com a localização do veículo facilitou muito a recuperação, de verdade estava mais preocupado com as ferramentas de trabalho que consegui recuperar.</p>
+        </div>
+    </div>
+  </section>
+
+  <section class="layerEight">
+    <h3>CONTRATE AGORA</h3>
+    <div class="bloco">
+        <div class="box">
+            <h2>Plano 1</h2>
+            <h2>Adesão <span>R$250,00</span></h2>
+            <h2>Por R$100,00</h2>
+            <p>Prazo 12 meses</p>
+            <p>Mensal R$69,00</p>
+            <p>Pague Somente depois de Instalado e testado, agende sua instalação via WhatsApp, clique no botão abaixo</p>
+            <a href="https://api.whatsapp.com/send?phone=5511917227534&text=contratar plano 1" target="_blank"><button>Contratar via WhatsApp</button></a>
+        </div>
+        <div class="box">
+            <h2>Plano 2</h2>
+            <h2>Adesão <span>R$250,00</span></h2>
+              <h2>Por R$100,00</h2>
+            <p>Prazo 24 meses</p>
+            <p>Mensal R$65,00</p>
+            <p>Pague Somente depois de Instalado e testado, agende sua instalação via WhatsApp, clique no botão abaixo</p>
+            <a href="https://api.whatsapp.com/send?phone=5511917227534&text=contratar plano 2" target="_blank"><button>Contratar via WhatsApp</button></a>
+        </div>
+        <div class="box">
+            <h2>Plano 3</h2>
+            <h2>Adesão <span>R$250,00</span></h2>
+            <h2>Por R$100,00</h2>
+            <p>Prazo 36 meses</p>
+            <p>Mensal R$59,00</p>
+            <p>Pague Somente depois de Instalado e testado, agende sua instalação via WhatsApp, clique no botão abaixo</p>
+           
+          
+            <a href="https://api.whatsapp.com/send?phone=5511917227534&text=contratar plano 3" target="_blank"><button>Contratar via WhatsApp</button></a>
+        </div>
+    </div>
+
+    <h3>Instalação Residencial ou local de Trabalho, agende sua Instalação</h3>
+    <p>Ainda com Dúvidas - Tire Suas Dúvidas com Representante Autorizado - Atendimento via WhatsApp</p>
+    <p>Pague Somente depois de Instalado, funcionado, testado, você localizando o veículo pelo APP :) </br> Não perca tempo agende sua instalação botão acima</p>
+    <a href="https://api.whatsapp.com/send?phone=5511917227534&text=tenho duvidas" target="_blank"><button>Falar com Representante</button></a>
+
+
+  </section>
+
+  <footer>
+    Todos os Direitos Reservados &copy; Rastreadores INSAT - 2022
+  </footer>
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
